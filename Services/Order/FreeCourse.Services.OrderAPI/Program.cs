f20e41044671d9c47337d15bd566355ffaf468c8
Hasan Hüseyin Alav <hhsynalv@gmail.com>
2022-02-10 16:05:34 +0300

    Bugs fixed

--- Services/Order/FreeCourse.Services.OrderAPI/Program.cs
+++ Services/Order/FreeCourse.Services.OrderAPI/Program.cs
@@ -1,5 +1,8 @@
+using FreeCourse.Services.Order.Infrastructure;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -13,7 +16,16 @@ namespace FreeCourse.Services.OrderAPI
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var servieProvide = scope.ServiceProvider;
+                var dbContext = servieProvide.GetRequiredService<OrderDbContext>();
+                dbContext.Database.Migrate();
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
